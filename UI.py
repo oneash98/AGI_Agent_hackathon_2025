ce6200b4c3503dfd0c01552e37e3abae514c45fe
ONEASH <oneash082498@gmail.com>
2025-03-30 13:10:43 +0900

    API 키 입력 기능 추가

--- UI.py
+++ UI.py
@@ -1,19 +1,53 @@
 import streamlit as st
-from operator import itemgetter
 from main import test_function
 import os
 from datetime import datetime
+from dotenv import load_dotenv
+
+
+if 'API_KEY' not in st.session_state: # API key 담을 변수 설정
+	if os.path.exists('.env'): 	# 로컬에서 테스트 실행 시 API KEY 가져오기 
+		load_dotenv()
+		st.session_state.API_KEY = os.getenv("API_KEY")
+	else: # 스트림릿 웹에서 실행 시
+		st.session_state.API_KEY = ""
+	
+if 'masked_API_KEY' not in st.session_state:
+	st.session_state.masked_API_KEY = ""
 
 # 웹 탭 꾸미기
 st.set_page_config(
     page_title="제목 추천받아요",
 )
 
-
 # UI 구현
 st.title("뭐라고 적을까요")
 st.subheader('뭐라고 적을까요 2')
 st.markdown('뭐라고 적을까요 3')
+
+# API 키 설정
+with st.sidebar:
+
+	def on_submit(): # API key submit 버튼 클릭 시
+		st.session_state.API_KEY = st.session_state.temp_key # api key
+		# api key 마스킹
+		temp_chars = list(st.session_state.API_KEY)
+		for i in range(len(temp_chars[4:-1])):
+			temp_chars[i+4] = "*"
+		st.session_state.masked_API_KEY = "".join(temp_chars)
+
+		st.session_state.temp_key = ""
+	
+	# API key 입력 form
+	with st.form("api_key_form", clear_on_submit=False):
+		intput_API_KEY = st.text_input(
+			label = f"Upstage API Key: {st.session_state.masked_API_KEY}",
+			placeholder = "Upstage API Key를 입력하세요",
+			key = "temp_key",
+		)
+
+		btn_api_key_submit = st.form_submit_button("Submit", on_click=on_submit)
+
 
 # 파일 업로드
 uploaded_file = st.file_uploader("파일을 선택하세요", type=['pdf', 'png', 'jpeg'])
@@ -25,7 +59,6 @@ if uploaded_file:
 
 	current_time = datetime.now()
 	current_time = current_time.isoformat().replace(":", "").replace("-", "").replace(".", "")
-	print(current_time)
 	temp = uploaded_file.name.split('.')
 	file_name = temp[0]
 	file_extension = temp[-1]
@@ -35,5 +68,5 @@ if uploaded_file:
 		f.write(uploaded_file.getbuffer())
     
 	# API 호출
-	text = test_function(file_path)
+	text = test_function(st.session_state.API_KEY, file_path)
 	st.markdown(text)
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-import os
-from dotenv import load_dotenv
 import requests
-import streamlit as st
 from bs4 import BeautifulSoup
 
-# API KEY 가져오기 
-if os.path.exists('.env'): # 로컬에서 테스트 실행
-    load_dotenv()
-    API_KEY = os.getenv("API_KEY")
-else: # streamlit web에서 실행
-    API_KEY = st.secrets["API_KEY"]
+# upstage request 오류 확인
+def print_upstage_error(response):
+    if 'error' in response.json().keys():
+        return response.json()['error']
 
-
-def test_function(file_path):
+# 테스트 함수
+def test_function(API_KEY, file_path):
 
     url = "https://api.upstage.ai/v1/document-digitization"
     headers = {"Authorization": f"Bearer {API_KEY}"}
     files = {"document": open(file_path, "rb")}
     data = {"ocr": "force", "base64_encoding": "['table']", "model": "document-parse"}
     response = requests.post(url, headers=headers, files=files, data=data)
+
+    # upstage 오류 확인
+    upstage_error = print_upstage_error(response)
+    if upstage_error:
+        return upstage_error
     
+    # 메인 기능
     contents_html = response.json()['content']['html']
     soup = BeautifulSoup(contents_html)
     text = soup.find(attrs = {'id': '20'}).text
